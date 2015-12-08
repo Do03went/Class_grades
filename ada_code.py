046bf5dce4814c426af43697aa5c58944c0f07a8
Devon Wentworth <DevonWentworth@Devons-MacBook-Pro-2.local>
2015-12-08 10:50:50 -0500

    ada code

---
+++ ada_code.py
@@ -0,0 +1,48 @@
+Python 2.7.10 |Anaconda 2.3.0 (x86_64)| (default, May 28 2015, 17:04:42) 
+[GCC 4.2.1 (Apple Inc. build 5577)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> def read_data(infile):
+
+	return infile="grades0.csv"
+
+SyntaxError: invalid syntax
+>>> def read_data(infile):
+
+	return infile="grades0.csv"
+SyntaxError: invalid syntax
+>>> def read_data(infile):
+
+	return infile"grades0.csv"
+SyntaxError: invalid syntax
+>>> 
+>>> def read_data(infile):
+
+	return "grades0.csv"
+
+>>> 
+>>> 
+>>> def read_data(infile):
+
+	return "grades0.csv"
+print infile
+SyntaxError: invalid syntax
+>>> def read_data(infile):
+
+	return "grades0.csv"
+print read_data
+SyntaxError: invalid syntax
+>>> 
+>>> def read_data(infile):
+
+	return "grades0.csv"
+
+>>> def read_data(infile):
+	grades = "grades0.csv"
+
+	return grades
+
+>>> 
+>>> def read_data(infile):
+	grades = "grades0.csv"
+
+	return grades
